02eef6101755dcc42f5639b06f09a23699bc4dee
Kenneth Ao <kao@csumb.edu>
2026-06-03 22:19:12 -0700

    recursion done

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,17 @@
-const permutations = function() {
-  
+const permutations = function(arr, index= 0, res = []) {
+    if (index == arr.length) {
+        res.push([...arr]);
+        return res;
+    }
+
+    for (let i = index; i < arr.length; i++) {
+        [arr[index], arr[i]] = [arr[i], arr[index]];
+        permutations(arr, index + 1, res);
+        [arr[index], arr[i]] = [arr[i], arr[index]];
+    }
+
+    return res;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,16 @@
-const pascal = function() {
-  
+const pascal = function(n, prev = [1]) {
+    if (n === 1) {
+        return prev;
+    }
+    let curr = [];
+    curr.push(prev[0]);
+    for (let i = 1; i < prev.length; i++) {
+        curr.push(prev[i - 1] + prev[i])
+    }
+
+    curr.push(prev[prev.length - 1]);
+    return pascal(n - 1, curr);
+
 };
   
 // Do not edit below this line
