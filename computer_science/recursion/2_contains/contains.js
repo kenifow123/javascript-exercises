06913f1099d016e1b2580fc2325994ac5d63b817
Kenneth Ao <kao@csumb.edu>
2026-06-03 21:01:09 -0700

    recursion 3 done

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,14 @@
-const contains = function() {
-  
+const contains = function(obj, find) {
+    const values = Object.values(obj);
+    if (values.includes(find)) return true;
+
+    const nestedObjs = values.filter(
+        value => typeof value === 'object' && value !== null
+    );
+
+    return nestedObjs.some(nestedObj => contains(nestedObj, find) === true);
 };
+
   
 // Do not edit below this line
 module.exports = contains;
--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,24 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(obj) {
+    let nestedObjs;
+    let count = 0;
+
+    if (typeof obj === 'object' && obj !== null) {
+        nestedObjs = Object.values(obj);
+    } else {
+        return;
+    }
+
+    for (const el of nestedObjs) {
+        if (Number.isInteger(el)) {
+            count++;
+        } else if (typeof el === 'object' && el !== null) {
+            count += totalIntegers(el);
+        }
+    }
+
+    return count;
+
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
