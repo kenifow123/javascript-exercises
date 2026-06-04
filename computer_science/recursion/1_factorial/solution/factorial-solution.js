6d6dc5841374bfe124205358cb6c9a56df4baaf8
Kenneth Ao <kao@csumb.edu>
2026-06-03 20:15:51 -0700

    test

--- computer_science/recursion/1_factorial/solution/factorial-solution.js
+++ computer_science/recursion/1_factorial/solution/factorial-solution.js
@@ -2,6 +2,7 @@ const factorial = function(n) {
   if (!Number.isInteger(n) || n < 0) return;
   if (n === 0) return 1;
   return n * factorial(n - 1);
+  //awefaew
 };
 
 module.exports = factorial;
